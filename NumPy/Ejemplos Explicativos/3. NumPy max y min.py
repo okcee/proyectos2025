bbac035558b17a4ebfe04dedc7f2b741c556de30
David Saavedra Varela <okcee@hotmail.com>
2025-04-14 16:24:28 +0200

    d140425 .2

--- NumPy/Ejemplos Explicativos/3. NumPy max y min.py
+++ NumPy/Ejemplos Explicativos/3. NumPy max y min.py
@@ -1,1 +1,16 @@
+import numpy as np
+
 # Averiguar valores máximos y mínimos de arrays
+array1 = np.random.randint(1, 100, 50)
+print("El array aleatorio generado es:/n",array1)
+print(type(array1))
+
+valor_maximo = array1.max() # Saber el valor máximo en el array, el número de mayor valor.
+posicion_valor_maximo = array1.argmax() # Saber la posición del valor máximo.
+print("El valor máximo es:",valor_maximo)
+print("La posición del valor máximo es:",posicion_valor_maximo)
+
+valor_minimo = array1.min() # Saber el valor mínimo en el arrya, el número de menor valor.
+posicion_valor_minimo = array1.argmin() # Saber la posición del valor mínimo.
+print("El valor mínimo es:",valor_minimo)
+print("La posición del valor mínimo es:",posicion_valor_minimo)
---
+++ NumPy/Ejemplos Explicativos/4. NumPy Indexación.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+# Numpy: Indexación en arrays
