58822b3869cddb1bd41dfcb03696991326770507
David Saavedra Varela <okcee@hotmail.com>
2025-04-20 14:06:37 +0200

    d200425 .1

--- Gestión de Inventario con SQL/gestion_inventario.py
+++ Gestión de Inventario con SQL/gestion_inventario.py
@@ -7,23 +7,19 @@ Este patrón se utiliza para acceder a la información de una entidad de nuestra
 En este caso a través del cliente DAO vamos a acceder a la información de la tabla cliente y poder mediante métodos de la clase Cliente DAO, interactuar con la clase Cliente para ejecutar sentencias en la tabla cliente
 Esta forma de relacionar los datos es el principio de aplicaciones que se conocen como ORM (Object Relational Mappin), o también de mapeo relación entidad
 '''
+import sqlite3
 
-'''Sistema de Gestión de Contactos/ # Raíz del proyecto
-├── gestion_inventario.py                     # Contiene la clase GestionInventario. Implementa operaciones CRUD
-│   ├── agregar un producto
-│   ├── mostrar todos los productos
-│   ├── buscar un producto
-│   ├── actualizar un producto
-│   └── eliminar un producto
-'''
 from conexion import Conexion
 from producto import Producto
+from mysql.connector import Error # Importar Error para manejo específico
+
 
 class GestionInventario:
-    CREATE = 'INSERT * FROM producto ORDER BY id' # Define una constante con la consulta SQL para seleccionar todos los registros de la tabla 'inventario' ordenados por 'id'.
-    READ = 'SELECT * FROM producto ORDER BY id'  # Define una constante con la consulta SQL para seleccionar todos los registros de la tabla 'producto' ordenados por 'id'.
-    UPDATE = 'UPDATE producto SET nombre=%s, apellido=%s, membresia=%s WHERE id=%s'  # Define una constante con la consulta SQL para actualizar un registro existente en la tabla 'producto'.
-    DELETE = 'DELETE FROM producto WHERE id=%s'  # Define una constante con la consulta SQL para eliminar un registro de la tabla 'producto' por su 'id'.
+    CREATE = 'INSERT * FROM producto ORDER BY nombre' # Define una constante con la consulta SQL para seleccionar todos los registros de la tabla 'inventario' ordenados por 'nombre'.
+    READ = 'SELECT * FROM producto ORDER BY nombre'  # Define una constante con la consulta SQL para seleccionar todos los registros de la tabla 'producto' ordenados por 'nombre'.
+    UPDATE = 'UPDATE producto SET nombre=%s, cantidad=%s, precio=%s, categoria=%s WHERE nombre==%s'  # Define una constante con la consulta SQL para actualizar un registro existente en la tabla 'inventario'.
+    DELETE = 'DELETE FROM producto WHERE nombre=%s'  # Define una constante con la consulta SQL para eliminar un registro de la tabla 'inventario' por su 'nombre'.
+    FIND = "SELECT id, nombre, cantidad, precio, categoria FROM producto WHERE nombre = %s"
     
     @classmethod
     def create(cls, producto):
@@ -62,7 +58,71 @@ class GestionInventario:
             if conexion is not None:  # Verifica si la variable 'conexion' tiene una conexión.
                 cursor.close()  # Cierra el cursor.
                 Conexion.liberar_conexion(conexion)  # Libera la conexión a la base de datos utilizando el método 'liberar_conexion' de la clase 'Conexion'.
+
+    @classmethod
+    def find_by_name(cls, nombre_buscado: str):
+        conexion = None  # Inicializa la variable 'conexion' a 'None' para almacenar la conexión a la base de datos.
+        producto_encontrado = None  # Inicializamos a None para controlar si se encontró algo
+        cursor = None
+        try:  # Inicia un bloque 'try' para manejar posibles excepciones.
+            conexion = Conexion.obtener_conexion()  # Obtiene una conexión a la base de datos utilizando el método 'obtener_conexion' de la clase 'Conexion'.
+            if conexion and conexion.is_connected(): # Buena práctica verificar conexión
+                cursor = conexion.cursor() # Crea un objeto cursor para ejecutar consultas SQL
+                valores = (nombre_buscado,) # Pasar el nombre_buscado como una tupla con un argumento
+                cursor.execute(cls.FIND, valores)
+                registro = cursor.fetchone() # Usar fetchone() para obtener un solo registro o None
+                if registro:
+                    # Crear el objeto Producto a partir de la tupla devuelta
+                    # Asumiendo el orden: id, nombre, cantidad, precio, categoria
+                    producto_encontrado = Producto(id=registro[0], nombre=registro[1], cantidad=registro[2], precio=registro[3], categoria=registro[4])
+            else:
+                print("Error: No se pudo obtener conexión para buscar por nombre.")
+        
+        except Error as e:
+            print(f'Ocurrió un error de base de datos al buscar el producto: {e}')
+        except Exception as e:  # Captura cualquier excepción que ocurra dentro del bloque 'try'.
+            print(f'Ocurrió un error al buscar el producto: {e}')  # Imprime un mensaje de error con la excepción capturada.
+        finally:  # Bloque 'finally' que se ejecuta siempre, haya o no excepciones.
+            if conexion is not None:  # Verifica si la variable 'conexion' tiene una conexión.
+                cursor.close()  # Cierra el cursor.
+                Conexion.liberar_conexion(conexion)  # Libera la conexión a la base de datos utilizando el método 'liberar_conexion' de la clase 'Conexion'.
+        return producto_encontrado  # Retorna la lista de objetos 'Producto' o None si no lo encuentra.
     
     @classmethod
     def update(cls, producto):
-        
+        conexion = None
+        try:
+            conexion = Conexion.obtener_conexion() # Obtiene una conexión a la base de datos utilizando el método 'obtener_conexion' de la clase 'Conexion'.
+            cursor = conexion.cursor() # Crea un objeto cursor para ejecutar consultas SQL.
+            valores = (producto.nombre, producto.apellido, producto.membresia, producto.id) # Especifica los valores para hacer el UPDATE, tomando los valores del objeto producto
+            cursor.execute(cls.UPDATE, valores) # Ejecuta la consulta SQL definida en el atributo constante 'UPDATE'.
+            conexion.commit() # Confirma la subida los cambios en la base de datos.
+            return cursor.rowcount # Está variable indica cuatos datos se modificaron en la base de datos
+        except Exception as e:
+            print(f'Ocurrió un error al insertar el producto: {e}')
+        finally:
+            if conexion is not None:  # Verifica si la variable 'conexion' tiene una conexión.
+                cursor.close()  # Cierra el cursor.
+                Conexion.liberar_conexion(conexion)  # Libera la conexión con la db
+    
+    @classmethod
+    def delete(cls, producto):
+        conexion = None
+        try: # Inicia un bloque 'try' para manejar posibles excepciones.
+            conexion = Conexion.obtener_conexion() # Obtiene una conexión a la base de datos utilizando el método 'obtener_conexion' de la clase 'Conexion'.
+            cursor = conexion.cursor() # Crea un objeto cursor para ejecutar consultas SQL.
+            valores = (producto.id,) # Especifica el valor de "ID" para ejecutar la sentencia delete, tomando los valores del objeto producto (como valores es una tupla, añadimos la "," después del primer valor para especificar que es una tupla con solo un dato)
+            cursor.execute(cls.DELETE, valores) # Ejecuta la consulta SQL definida en el atributo constante 'DELETE'.
+            conexion.commit() # Confirma la subida del cambio en la base de datos.
+            return cursor.rowcount # Está variable indica cuatos datos se modificaron en la base de datos
+        except Exception as e:
+            print(f'Ocurrió un error al insertar el producto: {e}')
+        finally:  # Bloque 'finally' que se ejecuta siempre, haya o no excepciones.
+            if conexion is not None:  # Verifica si la variable 'conexion' tiene una conexión.
+                cursor.close()  # Cierra el cursor.
+                Conexion.liberar_conexion(conexion)  # Libera la conexión a la base de datos
+    
+if __name__ == '__main__':  # Verifica si el script se está ejecutando directamente (no importado como módulo).
+    clientes = GestionInventario.read()  # Llama al método 'read' de la clase 'GestionInventario' para obtener todos los clientes.
+    for cliente in clientes:  # Itera sobre la lista de clientes obtenida.
+        print(cliente)  # Imprime la información de cada cliente (utilizando el método '__str__' de la clase 'Cliente').
--- Gestión de Inventario con SQL/inventarioAPP.py
+++ Gestión de Inventario con SQL/inventarioAPP.py
@@ -1,3 +1,4 @@
+# Contiene la lógica del menú y la interacción principal
 '''Sistema de Gestión de Contactos/ # Raíz del proyecto
 ├── inventarioAPP.py                          # Contiene la lógica del menú y la interacción principal
 │   ├── agregar un producto
@@ -5,18 +6,56 @@
 │   ├── buscar un producto
 │   ├── actualizar un producto
 │   └── eliminar un producto
-├── gestion_inventario.py                     # Contiene la clase GestionInventario. Implementa operaciones CRUD
-│   ├── agregar un producto
-│   ├── mostrar todos los productos
-│   ├── buscar un producto
-│   ├── actualizar un producto
-│   └── eliminar un producto
-├── producto.py
-│   ├── id de un producto                     # Contiene la clase Producto 
-│   ├── nombre de un producto
-│   ├── cantidad de un producto
-│   ├── precio de un producto
-│   └── categoria de un producto
-├── conexion.py                               # Contiene la clase Conexion, para MySQL
-└── inventario_db                             (Tabla en base de datos MySQL)
+salir
 '''
+
+from gestion_inventario import GestionInventario
+from producto import Producto
+
+print('~~~~~~ Menú: Sistema de Gestión de Inventario con Base de Datos ~~~~~~')
+opcion = None
+while opcion !=6:
+    print(f'''\n MENU:
+            1. Agregar un producto
+            2. Mostrar todos los productos
+            3. Buscar un producto
+            4. Actualizar datos de un producto
+            5. Eliminar un producto
+            6. Salir ''')
+    opcion = int(input('Ingrese una opción (1-6): '))
+    if opcion == 1: # Agregar un producto
+        nombre_var = input('Ingrese el nombre del producto: ')
+        cantidad_var = input('Ingrese el apellido del producto: ')
+        precio_var = input('Ingrese la membresia del producto: ')
+        categoria_var = input('Ingrese la membresia del producto: ')
+        producto = Producto(nombre=nombre_var, cantidad=cantidad_var, precio=precio_var, categoria=categoria_var)
+        productos_insertados = GestionInventario.create(producto)
+        print(f'Productos insertados: {productos_insertados}')
+    elif opcion == 2: # Mostrar todos los productos
+        productos = GestionInventario.read()
+        print('\n*** Listado de productos ***')
+        for producto in productos:
+            print(producto)
+    elif opcion == 3: # Buscar un producto
+        nombre_a_buscar = input("Introduce el nombre del producto que deseas buscar: ")
+        producto = Producto.find_by_name(nombre_a_buscar)
+        if producto:
+            print("\nProducto encontrado:")
+            print(f"Nombre: {producto.nombre}, Cantidad: {producto.cantidad}, Precio: {producto.precio}, Categoría: {producto.categoria}")
+        else:
+            print(f"\nNo se encontró ningún producto con el nombre '{nombre_a_buscar}'.")
+    elif opcion == 4: # Actualizar datos de un producto
+        id_producto_var = int(input('Ingrese el id del producto a modificar: '))
+        nombre_var = input('Ingrese el nombre del producto a modificar: ')
+        cantidad_var = input('Ingrese el apellido del producto a modificar: ')
+        precio_var = input('Ingrese la membresia del producto a modificar: ')
+        categoria_var = input('Ingrese la membresia del producto a modificar: ')
+        producto = Producto(Producto(id=id_producto_var, nombre=nombre_var, cantidad=cantidad_var, precio=precio_var, categoria=categoria_var))
+        producto_actualizado = GestionInventario.actualizar(producto)
+    elif opcion == 5: # Eliminar un producto
+        id_producto_var = int(input('Ingrese el id del producto a eliminar: '))
+        producto = Producto(id=id_producto_var)
+        producto_eliminados = GestionInventario.eliminar(producto)
+        print(f'Producto eliminado: {producto_eliminados}')
+else:
+    print("Salimos de la aplicación")
