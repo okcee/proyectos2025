0c814c9953f2d9c81ae86c4a0305e2a12be784bb
David Saavedra Varela <okcee@hotmail.com>
2025-04-21 21:07:14 +0200

    d210425 .2

--- Matplotlib/1. Matplotlib Ejemplos.py
+++ Matplotlib/1. Matplotlib Ejemplos.py
@@ -9,5 +9,39 @@ print('La lista "x" es:', x)
 print('La lista "y" es:', y)
 
 # Representar las listas mediante un gráfico
+plt.plot(x,y, 'red') # define la gráfica  y tiene atributo color rojo para la línea
+plt.title("Gráfico 1: Representación de X y su cuadrado") # Ponerle un título
+plt.xlabel("Eje x") # Nombre para la primera lista de valores Eje x.
+plt.ylabel("Eje y") # Nombre para la segunda lista de valores. Eje y.
+plt.show()
+plt.close()
 
-plt.plot(x,y)
+# Representar dos gráficos en el mismo espacio
+plt.subplot(1,2,1) # Atributos: Número de filas, Número de columnas y Posición
+plt.plot(x,y, 'green')
+plt.xlabel("Eje x") # Nombre para la primera lista de valores Eje x.
+plt.ylabel("Eje y") # Nombre para la segunda lista de valores. Eje y.
+
+plt.subplot(1,2,2)
+plt.plot(y,x, 'blue')
+plt.xlabel("Eje x") # Nombre para la primera lista de valores Eje x.
+plt.ylabel("Eje y") # Nombre para la segunda lista de valores. Eje y.
+
+plt.show()
+plt.close()
+
+# Representar dos gráficos: uno encima del otro
+plt.subplot(2,1,1) # Atributos: Número de filas, Número de columnas y Posición
+plt.plot(x,y, 'pink')
+plt.xlabel("Eje x") # Nombre para la primera lista de valores Eje x.
+plt.ylabel("Eje y") # Nombre para la segunda lista de valores. Eje y.
+
+plt.subplot(2,1,2)
+plt.plot(y,x, 'violet')
+plt.xlabel("Eje x") # Nombre para la primera lista de valores Eje x.
+plt.ylabel("Eje y") # Nombre para la segunda lista de valores. Eje y.
+
+plt.show()
+plt.close()
+
+# 
