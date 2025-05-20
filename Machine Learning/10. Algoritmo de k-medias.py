6ffce20f0aad0bbb5f097934090e6de9fadf16dc
David Saavedra Varela <okcee@hotmail.com>
2025-05-20 20:26:09 +0200

    d200525 .4

---
+++ Machine Learning/10. Algoritmo de k-medias.py
@@ -0,0 +1,8 @@
+# Algoritmo de k-medias
+'''
+El algoritmo de k medias el algoritmo de k medias es un algoritmo de aprendizaje no supervisado para resolver el problema de la clusterización.  
+Tiene como objetivo la partición de un conjunto de n observaciones en k grupos en el que cada observación pertenece al grupo cuyo valor medio es más cercano.  
+Es un método utilizado en minería de datos.  
+Aquí tienes un gráfico para distintas fases que pasa el algoritmo para conseguir agrupar las observaciones en tres grupos.  
+'''
+
