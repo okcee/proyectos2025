366cc226e44f926daee93d0fc3d191ee99117fe0
David Saavedra Varela <okcee@hotmail.com>
2025-05-20 19:27:57 +0200

    d200525 .1

--- Machine Learning/8. Random Forest.py
+++ Machine Learning/8. Random Forest.py
@@ -10,3 +10,30 @@ Alta Precisión: Es reconocido por ser uno de los algoritmos de clasificación m
 Manejo de Datos Grandes y Muchas Variables: Es eficiente y escalable para trabajar con grandes conjuntos de datos y puede gestionar eficazmente bases de datos con cientos o miles de variables de entrada sin necesidad de una selección de características previa exhaustiva.
 '''
 
+import pandas as pd
+
+vino = pd.read_csv("S:\\_proyectos2025\\Machine Learning\\Datasets\\vino.csv")
+
+# print(vino.head())
+
+X = vino.drop('Wine Type', axis=1)
+y = vino['Wine Type']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+from sklearn.ensemble import RandomForestClassifier
+
+ramdomforest = RandomForestClassifier(n_estimators=80) # Tenemos que pasar el número de árboles (estimadores) que va a hacer para estimar el resultado
+ramdomforest.fit(X_train, y_train)
+
+predicciones = ramdomforest.predict(X_test)
+print('Las predicciones son:\n', predicciones) # Predicciones
+print('Los valores reales conocidos son: \n', y_test)
+
+# Ahora valoraremos la precisión del modelo
+from sklearn.metrics import classification_report
+
+print('Precisión del modelo: \n', classification_report(y_test, predicciones))
+
